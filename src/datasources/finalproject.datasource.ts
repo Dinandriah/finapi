9508142d47b0862b42dac3a059a16ad52c5d0526
Dinandriah <dinandriah@gmail.com>
2018-12-12 08:56:50 -0700

    test

--- src/datasources/finalproject.datasource.ts
+++ src/datasources/finalproject.datasource.ts
@@ -1,6 +1,6 @@
 import {inject} from '@loopback/core';
 import {juggler} from '@loopback/repository';
-import * as config from './finalproject.datasource.json';
+import * as config from './finalproject.datasource.json'
 
 export class FinalprojectDataSource extends juggler.DataSource {
   static dataSourceName = 'finalproject';
